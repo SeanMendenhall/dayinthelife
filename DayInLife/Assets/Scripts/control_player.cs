d8f14242f319a8e88243c0edbce793c071674a37
amcolema <amcolema@ucsc.edu>
2014-05-27 18:46:53 -0700

    Initial commit

---
+++ DayInLife/Assets/Scripts/control_player.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class control_player : MonoBehaviour {
+	[HideInInspector]
+	public bool facingLeft = true;
+	[HideInInspector]
+	public bool jump = false;
+
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+	public float jumpForce = 1000f;
+
+	private Transform Grounded;
+	private bool grounded = false;
+
+	private Animator anim;
+
+	
+	// Use this for initialization
+	void Awake()
+	{
+		Grounded = transform.Find ("Grounded");
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		grounded = Physics2D.Linecast (transform.position, Grounded.position, 1 << LayerMask.NameToLayer ("Ground"));
+
+		if(Input.GetKeyDown(KeyCode.W) && grounded)
+			jump = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (Input.GetKey (KeyCode.D) && rigidbody2D.velocity.x < maxSpeed) 
+		{
+			rigidbody2D.AddForce (Vector2.right * moveForce);
+			if(facingLeft)
+				Flip();
+		}
+
+		if (Input.GetKey (KeyCode.A) && Mathf.Abs (rigidbody2D.velocity.x) < maxSpeed) 
+		{
+			rigidbody2D.AddForce (new Vector2(-1, 0) * moveForce);
+			if(!facingLeft)
+				Flip();
+		}
+
+		if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed)
+			rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+
+		if(jump)
+		{
+			rigidbody2D.AddForce (Vector2.up*jumpForce);
+
+			jump = false;
+		}
+
+		// punch
+		if (Input.GetKeyDown(KeyCode.J))
+		{
+			anim.Play ("punch");
+		}
+
+	}
+
+	void Flip()
+	{
+		facingLeft = !facingLeft;
+
+		Vector3 theScale = transform.localScale;
+
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
